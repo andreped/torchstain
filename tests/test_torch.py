7f12c8213a489e53f650b7f1848e9a47773c24ae
André Pedersen <andrped94@gmail.com>
2025-01-11 11:59:39 +0100

    Added test_multitarget_macenko_torch unit test

--- tests/test_torch.py
+++ tests/test_torch.py
@@ -21,7 +21,7 @@ def test_cov():
 def test_percentile():
     x = np.random.randn(10, 10)
     p = 20
-    p_np = np.percentile(x, p, interpolation='nearest')
+    p_np = np.percentile(x, p, interpolation="nearest")
     p_t = torchstain.torch.utils.percentile(torch.tensor(x), p)
 
     np.testing.assert_almost_equal(p_np, p_t)
@@ -35,15 +35,15 @@ def test_macenko_torch():
     # setup preprocessing and preprocess image to be normalized
     T = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: x * 255)
+        transforms.Lambda(lambda x: x * 255),
     ])
     t_to_transform = T(to_transform)
 
     # initialize normalizers for each backend and fit to target image
-    normalizer = torchstain.normalizers.MacenkoNormalizer(backend='numpy')
+    normalizer = torchstain.normalizers.MacenkoNormalizer(backend="numpy")
     normalizer.fit(target)
 
-    torch_normalizer = torchstain.normalizers.MacenkoNormalizer(backend='torch')
+    torch_normalizer = torchstain.normalizers.MacenkoNormalizer(backend="torch")
     torch_normalizer.fit(T(target))
 
     # transform
@@ -71,10 +71,10 @@ def test_reinhard_torch():
     t_to_transform = T(to_transform)
 
     # initialize normalizers for each backend and fit to target image
-    normalizer = torchstain.normalizers.ReinhardNormalizer(backend='numpy')
+    normalizer = torchstain.normalizers.ReinhardNormalizer(backend="numpy")
     normalizer.fit(target)
 
-    torch_normalizer = torchstain.normalizers.ReinhardNormalizer(backend='torch')
+    torch_normalizer = torchstain.normalizers.ReinhardNormalizer(backend="torch")
     torch_normalizer.fit(T(target))
 
     # transform
@@ -86,5 +86,36 @@ def test_reinhard_torch():
     result_torch = result_torch.numpy().astype("float32") / 255.
 
     
+    # assess whether the normalized images are identical across backends
+    np.testing.assert_almost_equal(result_numpy.flatten(), result_torch.flatten(), decimal=2, verbose=True)
+
+def test_multitarget_macenko_torch():
+    size = 1024
+    curr_file_path = os.path.dirname(os.path.realpath(__file__))
+    target = cv2.resize(cv2.cvtColor(cv2.imread(os.path.join(curr_file_path, "../data/target.png")), cv2.COLOR_BGR2RGB), (size, size))
+    to_transform = cv2.resize(cv2.cvtColor(cv2.imread(os.path.join(curr_file_path, "../data/source.png")), cv2.COLOR_BGR2RGB), (size, size))
+
+    # setup preprocessing and preprocess image to be normalized
+    T = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x * 1),
+    ])
+    t_to_transform = T(to_transform)
+
+    # initialize normalizers for each backend and fit to target image
+    normalizer = torchstain.normalizers.MultiMacenkoNormalizer(backend="numpy", norm_mode="avg-post")
+    normalizer.fit(target)
+
+    torch_normalizer = torchstain.normalizers.MultiMacenkoNormalizer(backend="torch", norm_mode="avg-post")
+    torch_normalizer.fit(T(target))
+
+    # transform
+    result_numpy, _, _ = normalizer.normalize(I=to_transform, stains=True)
+    result_torch, _, _ = torch_normalizer.normalize(I=t_to_transform, stains=True)
+
+    # convert to numpy and set dtype
+    result_numpy = result_numpy.astype("float32") / 255.
+    result_torch = result_torch.numpy().astype("float32") / 255.
+
     # assess whether the normalized images are identical across backends
     np.testing.assert_almost_equal(result_numpy.flatten(), result_torch.flatten(), decimal=2, verbose=True)
